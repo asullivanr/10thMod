0ccf80c07e97daf78c9ed595db44f36f3ee921d9
Andrew Sullivan <603788+asullivanr@users.noreply.github.com>
2022-10-11 13:13:29 -0500

    Update config.cpp

--- addons/Tenthed_Core/config.cpp
+++ addons/Tenthed_Core/config.cpp
@@ -9,3 +9,46 @@ class CfgPatches {
         units[] = {};
     };
 };
+class CfgFactionClasses {
+    class Tenthed_UnitFact_Main {
+        displayname = "[10th] ODST";
+        priority = 1;
+        side = 1;
+        icon = "\Tenthed_Core\data\logo.paa";
+        flag = "\Tenthed_Core\data\logo.paa";
+    };
+};
+class CfgEditorCategories {
+    class Tenthed_EditorCategory_Objects {
+        displayName = "[10th] ODST";
+    };
+    class Tenthed_EditorCategory_Turrets {
+        displayName = "[10th] Turrets";
+    };
+    class Tenthed_EditorCategory_Items {
+        displayName = "[10th] Items";
+    };
+};
+class CfgEditorSubcategories {
+    class Tenthed_EditorSubcategory_Objects_Military {
+        displayName = "Military";
+    };
+    class Tenthed_EditorSubcategory_Objects_Banners {
+        displayName = "Banners";
+    };
+    class Tenthed_EditorSubcategory_Objects_Weapons {
+        displayName = "Weapons";
+    };
+    class Tenthed_EditorSubcategory_Objects_Backpacks {
+        displayName = "Backpacks";
+    };
+    class Tenthed_EditorSubcategory_Objects_Turrets {
+        displayName = "Turrets";
+    };
+    class Tenthed_EditorSubcategory_Objects_Supplies {
+        displayName = "Supplies";
+    };
+    class Tenthed_EditorSubcategory_Objects_Vehicles {
+        displayName = "Vehicles";
+    };
+};
