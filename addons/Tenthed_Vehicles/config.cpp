b996754a850a47e916893c834404b67bfbef30a8
Andrew Sullivan <603788+asullivanr@users.noreply.github.com>
2022-10-12 16:02:25 -0500

    Update config.cpp
    Fixed textures and categories.

--- addons/Tenthed_Vehicles/config.cpp
+++ addons/Tenthed_Vehicles/config.cpp
@@ -7,6 +7,16 @@ class CfgPatches {
 			"OPTRE_Core"
 		};
 		weapons[] = {};
+		units[] = {
+			"Tenthed_Pelican",
+			"Archangel_Pelican",
+			"Dodo_Pelican",
+			"beeHornet",
+			"ZakuHornet",
+			"ARES_Tank",
+			"ARES_MGS",
+			"Zeus_Stallion"
+		};
 	};
 };
 class CfgVehicles {
@@ -21,11 +31,12 @@ class CfgVehicles {
 		  author = "PocketsTheWizard";
 		  scope = 2;
 		  scopeCurator = 2;
+		  faction = "Tenthed_EditorCategory_Objects";
 		  editorCategory = "Tenthed_EditorCategory_Objects";
-		  editorSubcategory = "Tenthed_EditorSubcategory_Objects_Vehicles";
+		  editorSubcategory = "Tenthed_EditorSubcategory_Objects_Pelicans";
 		  hiddenSelectionsTextures[] =
 		  {
-			  "10th_AC\data\vehicles\10th_PelicanExterior.paa",
+			  "\Tenthed_Vehicles\data\10th_pelican\10th_PelicanExterior.paa",
 			  ""
 		  };
 	};
@@ -34,11 +45,12 @@ class CfgVehicles {
 		author = "PocketsTheWizard";
 		scope = 2;
 		scopeCurator = 2;
+		faction = "Tenthed_EditorCategory_Objects";
 		editorCategory = "Tenthed_EditorCategory_Objects";
-		editorSubcategory = "Tenthed_EditorSubcategory_Objects_Vehicles";
+		editorSubcategory = "Tenthed_EditorSubcategory_Objects_Pelicans";
 		hiddenSelectionsTextures[] =
 		{
-			"10th_AC\data\vehicles\archanglePelican.paa",
+			"\Tenthed_Vehicles\data\archangel_pelican\archanglePelican.paa",
 			""
 		};
 	};
@@ -47,11 +59,12 @@ class CfgVehicles {
 		author = "PocketsTheWizard";
 		scope = 2;
 		scopeCurator = 2;
+		faction = "Tenthed_EditorCategory_Objects";
 		editorCategory = "Tenthed_EditorCategory_Objects";
-		editorSubcategory = "Tenthed_EditorSubcategory_Objects_Vehicles";
+		editorSubcategory = "Tenthed_EditorSubcategory_Objects_Pelicans";
 		hiddenSelectionsTextures[] =
 		{
-			"10th_AC\data\vehicles\DodoPelican.paa",
+			"\Tenthed_Vehicles\data\dodo_pelican\DodoPelican.paa",
 			""
 		};
 	};
@@ -60,23 +73,25 @@ class CfgVehicles {
 		scopeCurator = 2;
 		author = "PcoketsTheWizard";
 		displayName = "b(ee)Hornet";
+		faction = "Tenthed_EditorCategory_Objects";
 		editorCategory = "Tenthed_EditorCategory_Objects";
-		editorSubcategory = "Tenthed_EditorSubcategory_Objects_Vehicles";
+		editorSubcategory = "Tenthed_EditorSubcategory_Objects_Hornets";
 		hiddenSelectionsTextures[] =
 		{
-			"10th_AC\data\vehicles\beeHornet.paa"
+			"\Tenthed_Vehicles\data\bee_hornet\beeHornet.paa"
 		};
 	};
-	class ZakuHornet1 : OPTRE_UNSC_hornet_green_CAP {
+	class ZakuHornet : OPTRE_UNSC_hornet_green_CAP {
 		scope = 2;
 		scopeCurator = 2;
 		author = "PcoketsTheWizard";
 		displayName = "Red Comet Hornet";
+		faction = "Tenthed_EditorCategory_Objects";
 		editorCategory = "Tenthed_EditorCategory_Objects";
-		editorSubcategory = "Tenthed_EditorSubcategory_Objects_Vehicles";
+		editorSubcategory = "Tenthed_EditorSubcategory_Objects_Hornets";
 		hiddenSelectionsTextures[] =
 		{
-			"10th_AC\data\vehicles\ZakuHornet.paa"
+			"\Tenthed_Vehicles\data\zaku_hornet\ZakuHornet.paa"
 		};
 	};
 	class ARES_Tank : OPTRE_M808S {
@@ -84,6 +99,7 @@ class CfgVehicles {
 		scopeCurator = 2;
 		author = "PcoketsTheWizard";
 		displayName = "AresScorpion";
+		faction = "Tenthed_EditorCategory_Objects";
 		editorCategory = "Tenthed_EditorCategory_Objects";
 		editorSubcategory = "Tenthed_EditorSubcategory_Objects_Vehicles";
 		magazines[] =
@@ -103,13 +119,13 @@ class CfgVehicles {
 		};
 		hiddenSelectionsTextures[] =
 		{
-			"10th_AC\data\vehicles\body_lopo_DR.paa",
-			"10th_AC\data\vehicles\cing_dv_m808s_DR.paa",
-			"10th_AC\data\vehicles\cing_rt_m808s_DR.paa",
-			"10th_AC\data\vehicles\turretbase_lopo_DR.paa",
-			"10th_AC\data\vehicles\turrethead_lopo_DR.paa",
-			"10th_AC\data\vehicles\turretguns_lopo_DR.paa",
-			"10th_AC\data\vehicles\ghammer_lopo_DR.paa",
+			"\Tenthed_Vehicles\data\ares_tank\body_lopo_DR.paa",
+			"\Tenthed_Vehicles\data\ares_tank\cing_dv_m808s_DR.paa",
+			"\Tenthed_Vehicles\data\ares_tank\cing_rt_m808s_DR.paa",
+			"\Tenthed_Vehicles\data\ares_tank\turretbase_lopo_DR.paa",
+			"\Tenthed_Vehicles\data\ares_tank\turrethead_lopo_DR.paa",
+			"\Tenthed_Vehicles\data\ares_tank\turretguns_lopo_DR.paa",
+			"\Tenthed_Vehicles\data\ares_tank\ghammer_lopo_DR.paa",
 		};
 	};
 	class ARES_MGS : OPTRE_M413_MGS_UNSC_tan {
@@ -117,17 +133,18 @@ class CfgVehicles {
 		author = "PocketsTheWizard";
 		scope = 2;
 		scopeCurator = 2;
+		faction = "Tenthed_EditorCategory_Objects";
 		editorCategory = "Tenthed_EditorCategory_Objects";
 		editorSubcategory = "Tenthed_EditorSubcategory_Objects_Vehicles";
 		hiddenSelectionsTextures[] =
 		{
-			"10th_AC\data\vehicles\Bison_Body_tan_CO.paa",
-			"10th_AC\data\vehicles\Bison_Misc_tan_CO.paa",
-			"10th_AC\data\vehicles\Bison_WheelsCannon_tan_CO.paa",
-			"10th_AC\data\vehicles\Bison_Seats_CO.paa",
-			"10th_AC\data\vehicles\Bison_Interior_CO.paa",
-			"10th_AC\data\vehicles\Bison_MFCDScreens_CO.paa",
-			"10th_AC\data\vehicles\Bison_90mm_tan_CO.paa",
+			"\Tenthed_Vehicles\data\ares_mgs\Bison_Body_tan_CO.paa",
+			"\Tenthed_Vehicles\data\ares_mgs\Bison_Misc_tan_CO.paa",
+			"\Tenthed_Vehicles\data\ares_mgs\Bison_WheelsCannon_tan_CO.paa",
+			"\Tenthed_Vehicles\data\ares_mgs\Bison_Seats_CO.paa",
+			"\Tenthed_Vehicles\data\ares_mgs\Bison_Interior_CO.paa",
+			"\Tenthed_Vehicles\data\ares_mgs\Bison_MFCDScreens_CO.paa",
+			"\Tenthed_Vehicles\data\ares_mgs\Bison_90mm_tan_CO.paa",
 		};
 	};
 	class Zeus_Stallion : OPTRE_m1087_stallion_cover_unsc {
@@ -135,14 +152,15 @@ class CfgVehicles {
 		author = "Kodiak";
 		scope = 2;
 		scopeCurator = 2;
+		faction = "Tenthed_EditorCategory_Objects";
 		editorCategory = "Tenthed_EditorCategory_Objects";
 		editorSubcategory = "Tenthed_EditorSubcategory_Objects_Vehicles";
 		hiddenSelectionsTextures[] =
 		{
-			"10th_AC\data\vehicles\zeusstallion.paa",
-			"10th_AC\data\vehicles\Truck_ext02_standard_co.paa",
-			"10th_AC\data\vehicles\Truck_cargo_standard_co.paa",
-			"10th_AC\data\vehicles\Truck_cover_standard_co.paa",
+			"\Tenthed_Vehicles\data\zeus_stallion\zeusstallion.paa",
+			"\Tenthed_Vehicles\data\zeus_stallion\Truck_ext02_standard_co.paa",
+			"\Tenthed_Vehicles\data\zeus_stallion\Truck_cargo_standard_co.paa",
+			"\Tenthed_Vehicles\data\zeus_stallion\Truck_cover_standard_co.paa",
 		};
 	};
 }
