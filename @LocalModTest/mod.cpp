d75e440eeccdd45ae0ee3c240d9e8940c1dc5db7
Andrew Sullivan <603788+asullivanr@users.noreply.github.com>
2022-10-15 20:20:03 -0500

    Update mod.cpp

--- @LocalModTest/mod.cpp
+++ @LocalModTest/mod.cpp
@@ -1,8 +1,8 @@
 author = "Silver, Kodiak, Crow, Akira";
-name = "10th Armor Mod";
+name = "10th Mod";
 picture = "logo.png"; 
 logo = "logo.paa";
-overview = "10th ODST custom armors, weapons, vehicles and equipment mod. ";
+overview = "10th ODST custom armors, weapons, vehicles and equipment mod.";
 actionName = "GitHub";
 action = "https://github.com/asullivanr/10thArmorMod";
 version = 1;
