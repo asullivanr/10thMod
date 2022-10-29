b34e02e133173e20d7ce80450cde6f76c9cac37c
Andrew Sullivan <603788+asullivanr@users.noreply.github.com>
2022-10-29 17:30:04 -0500

    Update config.cpp
    Removed testing objects and added flags.

--- addons/Tenthed_Objects/config.cpp
+++ addons/Tenthed_Objects/config.cpp
@@ -10,61 +10,47 @@ class CfgPatches {
     };
 };
 class CfgVehicles {
-	class Strategic;
-	class Tenthed_Bunker_base : Strategic {
-		author = "$STR_A3_Bohemia_Interactive";
-		mapSize = 1;
-		_generalMacro = "Tenthed_Bunker_base";
-		scope = 0;
-		scopeCurator = 0;
-		displayName = "";
-		model = "\A3\Weapons_F\empty.p3d";
-		editorCategory = "EdCat_Structures_Altis";
-		editorSubcategory = "EdSubcat_Military";
-		vehicleClass = "Structures_Military";
-		destrType = "DestructBuilding";
-		accuracy = 1000;
-		armor = 1000; // Default: 500
-		class DestructionEffects {};
-	};
-	class Tenthed_BagBunker_Large_F : Tenthed_Bunker_base {
-		author = "$STR_A3_Bohemia_Interactive";
-		mapSize = 12.17;
-		class SimpleObject {
-			eden = 0;
-			animate[] = {};
-			hide[] = {};
-			verticalOffset = 0.88999999;
-			verticalOffsetWorld = 0;
-			init = "''";
+	class Flagpole_F;
+	class Sign_F;
+	class Tenthed_FlagBase : Flagpole_F {
+		model = "\OPTRE_Misc\Multiplayer\CTF_flag_pole";
+		armor = 999999;
+		armorStructural = 999;
+		scope = 1;
+		scopeCurator = 1;
+		displayName = "[10th] Flag (Base)";
+		editorCategory = "Tenthed_EditorCategory_Objects";
+		editorSubcategory = "Tenthed_EditorSubcategory_Objects_Military";
+		hiddenSelections[] = {
+			"camo1"
 		};
-		editorPreview = "\A3\EditorPreviews_F\Data\CfgVehicles\Land_BagBunker_Large_F.jpg";
-		_generalMacro = "Tenthed_BagBunker_Large_F";
+		hiddenSelectionsTextures[] = {
+			"optre_misc\multiplayer\data\flag_guidon_whiteunsc_co.paa"
+		};
+		hiddenSelectionsMaterials[] = {
+			"optre_misc\multiplayer\data\CTF_flag.rvmat"
+		};
+	};
+	class Tenthed_FlagStand : Sign_F {
+		model = "\OPTRE_Misc\Multiplayer\CTF_flag_stand";
+		vehicleClass = "OPTRE_City_Building_class";
+		armor = 999999;
+		armorStructural = 999;
 		scope = 2;
 		scopeCurator = 2;
-		displayName = "$STR_A3_CfgVehicles_Land_BagBunker_Large_F0";
-		model = "\A3\Structures_F\Mil\BagBunker\BagBunker_Large_F.p3d";
-		icon = "iconObject_1x1";
-		editorCategory = "EdCat_Structures_Altis";
+		displayName = "[10th] Flag (Stand)";
+		editorCategory = "Tenthed_EditorCategory_Objects";
+		editorSubcategory = "Tenthed_EditorSubcategory_Objects_Military";
 	};
-	class Tenthed_BagBunker_Small_F : Tenthed_Bunker_base {
-		author = "$STR_A3_Bohemia_Interactive";
-		mapSize = 5.6900001;
-		class SimpleObject {
-			eden = 0;
-			animate[] = {};
-			hide[] = {};
-			verticalOffset = 0.96600002;
-			verticalOffsetWorld = 0;
-			init = "''";
-		};
-		editorPreview = "\A3\EditorPreviews_F\Data\CfgVehicles\Land_BagBunker_Small_F.jpg";
-		_generalMacro = "Tenthed_BagBunker_Small_F";
+	class Tenthed_ODST_Flag : Tenthed_FlagBase {
 		scope = 2;
 		scopeCurator = 2;
-		displayName = "$STR_A3_CfgVehicles_Land_BagBunker_Small_F0";
-		model = "\A3\Structures_F\Mil\BagBunker\BagBunker_Small_F.p3d";
-		icon = "iconObject_1x1";
-		editorCategory = "EdCat_Structures_Altis";
+		displayName = "[10th] ODST Logo Flag";
+		hiddenSelections[] = {
+			"camo1"
+		};
+		hiddenSelectionsTextures[] = {
+			"optre_misc\multiplayer\data\flag_guidon_redODST_co.paa"
+		};
 	};
 };
