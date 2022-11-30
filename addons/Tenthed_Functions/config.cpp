c6833b57b29ad20a6018535eedd1160f25f6ccea
Andrew Sullivan <603788+asullivanr@users.noreply.github.com>
2022-11-29 18:57:11 -0600

    White listed Arsenal function

---
+++ addons/Tenthed_Functions/config.cpp
@@ -0,0 +1,27 @@
+class CfgPatches {
+    class Tenthed_functions {
+        author = "Akira";
+        name = "10th ODST: Functions";
+        requiredAddons[] = {
+            "OPTRE_Core",
+            "Tenthed_core"
+        };
+        weapons[] = {};
+        units[] = {};
+    };
+};
+class CfgFunctions {
+    class Tenthed_functions
+    {
+    	//Prefix for functions ie Tenthed_fnc_# (_fnc_ is added automatically between tag and file name see below)
+    	tag = "Tenthed";
+      
+        //Category
+        class Tenthed_Server_Functions
+        {
+            file="\Tenthed_Functions\functions";
+            class Arsenal {};
+            // call [ _this ] call Tenthed_fnc_Arsenal;
+        };
+    };
+};
