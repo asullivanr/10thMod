06d7792598075297a11edf88a74701d6fcf9bb93
Andrew Sullivan <603788+asullivanr@users.noreply.github.com>
2023-06-15 18:23:15 -0500

    Initial
    Created base classes for new factions.

---
+++ addons/Tenthed_Units/AuriganMilitary/config.cpp
@@ -0,0 +1,12 @@
+class CfgPatches {
+    class Tenthed_AuriganMilitary {
+        author = "Akira";
+        name = "10th ODST: Aurigan Military";
+        requiredVersion=0.1;
+        requiredAddons[] = {
+            "Tenthed_core",
+        };
+        weapons[] = {};
+        units[] = {};
+    };
+};
---
+++ addons/Tenthed_Units/UNSC_deserters/config.cpp
@@ -0,0 +1,12 @@
+class CfgPatches {
+    class Tenthed_UNSCDeserters {
+        author = "Akira";
+        name = "10th ODST: UNSC Deserters";
+        requiredVersion=0.1;
+        requiredAddons[] = {
+            "Tenthed_core",
+        };
+        weapons[] = {};
+        units[] = {};
+    };
+};
