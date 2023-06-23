9b1e70e6b75b06dcef8e364edfaeeae4ffce0df5
red2fred2 <admin@red2fred2.com>
2023-06-23 14:58:31 -0500

    Remove redundant pelican code I forgot to remove this when I moved the pelican code out to its own folder.

--- addons/Tenthed_Vehicles/config.cpp
+++ addons/Tenthed_Vehicles/config.cpp
@@ -8,194 +8,23 @@ class CfgPatches {
 		};
 		weapons[] = {};
 		units[] = {
-			"Tenthed_Pelican",
-			"Tenthed_Archangel_Pelican",
-			"Tenthed_Dodo_Pelican",
 			"Tenthed_beeHornet",
 			"Tenthed_ZakuHornet",
 			"Tenthed_ARES_Tank",
 			"Tenthed_ARES_MGS",
 			"Tenthed_Zeus_Stallion",
-			"Tenthed_Pelican_Desert",
-			"Tenthed_Pelican_Jungle",
-			"Tenthed_Pelican_Urban",
 			"Tenthed_Hornet",
-			"Tenthed_Pelican_SOCOM_Desert",
-			"Tenthed_Pelican_SOCOM_Jungle",
-			"Tenthed_Pelican_SOCOM_Urban",
-			"Tenthed_Pelican_Urban_fir_test",
 			"Tenthed_M494_Oryx"
 		};
 	};
 };
 class CfgVehicles {
-	class VES_D77HTCI_A;
 	class OPTRE_UNSC_hornet_green_CAP;
 	class OPTRE_M808S;
 	class OPTRE_M413_MGS_UNSC_tan;
 	class OPTRE_m1087_stallion_cover_unsc;
 
 	class Eventhandlers;
-
-	// Base type Pelican
-	class Tenthed_Pelican_Base : VES_D77HTCI_A {
-		displayName = "Tenthed_Pelican_Base";
-		author = "";
-		scope = 1;
-		scopeCurator = 1;
-		scopeArsenal = 1;
-		faction = "Tenthed_UnitFact_Main";
-		editorCategory = "Tenthed_EditorCategory_Main";
-		editorSubcategory = "Tenthed_EditorSubcategory_Objects_Pelicans";
-		hiddenSelectionsTextures[] = {
-			"\Tenthed_Vehicles\data\10th_pelican\10th_PelicanExterior.paa",
-			""
-		};
-		weapons[] = {
-			"CMFlareLauncher",
-			"OPTRE_missiles_Anvil1"
-		};
-		magazines[] = {
-			"300Rnd_CMFlare_Chaff_Magazine" // 168Rnd_CMFlare_Chaff_Magazine
-		};
-		class TransportBackpacks {
-			class _xx_TenthedMortarBackpack {
-				backpack = "Tenthed_Mortar_Backpack";
-				count = 6;
-			};
-			class _xx_TenthedHMGBackpack {
-				backpack = "Tenthed_HMG_Backpack";
-				count = 6;
-			};
-			class _xx_ParachuteBackpack {
-				backpack = "B_Parachute";
-				count = 6;
-			};
-		};
-		class TransportMagazines {};
-		class TransportWeapons {};
-		class TransportItems {};
-	};
-
-	// Pelicans
-	class Tenthed_Pelican_Desert : Tenthed_Pelican_Base {
-		displayName = "[10th] Pelican - Desert";
-		author = "Kodiak";
-		scope = 2;
-		scopeCurator = 2;
-		scopeArsenal = 2;
-		hiddenSelectionsTextures[] = {
-			"\Tenthed_Vehicles\data\Pelican_Variants\pelican_DESERT.paa",
-			""
-		};
-	};
-	class Tenthed_Pelican_Jungle : Tenthed_Pelican_Base {
-		displayName = "[10th] Pelican - Jungle";
-		author = "Kodiak";
-		scope = 2;
-		scopeCurator = 2;
-		scopeArsenal = 2;
-		hiddenSelectionsTextures[] = {
-			"\Tenthed_Vehicles\data\Pelican_Variants\pelican_JUNGLE.paa",
-			""
-		};
-	};
-	class Tenthed_Pelican_Urban : Tenthed_Pelican_Base {
-		displayName = "[10th] Pelican - Urban";
-		author = "Kodiak";
-		scope = 2;
-		scopeCurator = 2;
-		scopeArsenal = 2;
-		hiddenSelectionsTextures[] = {
-			"\Tenthed_Vehicles\data\Pelican_Variants\pelican_URBAN.paa",
-			""
-		};
-	};
-
-	class OPTRE_Pelican_armed_SOCOM;
-	class Tenthed_Pelican_SOCOM_Desert : OPTRE_Pelican_armed_SOCOM {
-		displayName = "[10th] Pelican SOCOM - Desert";
-		author = "Kodiak";
-		scope = 2;
-		scopeCurator = 2;
-		scopeArsenal = 2;
-
-		faction = "Tenthed_UnitFact_Main";
-		editorCategory = "Tenthed_EditorCategory_Main";
-		editorSubcategory = "Tenthed_EditorSubcategory_Objects_Pelicans";
-
-		hiddenSelectionsTextures[] = {
-			"\Tenthed_Vehicles\data\Pelican_Variants\pelican_DESERT.paa",
-			""
-		};
-	};
-	class Tenthed_Pelican_SOCOM_Jungle : OPTRE_Pelican_armed_SOCOM {
-		displayName = "[10th] Pelican SOCOM - Jungle";
-		author = "Kodiak";
-		scope = 2;
-		scopeCurator = 2;
-		scopeArsenal = 2;
-
-		faction = "Tenthed_UnitFact_Main";
-		editorCategory = "Tenthed_EditorCategory_Main";
-		editorSubcategory = "Tenthed_EditorSubcategory_Objects_Pelicans";
-
-		hiddenSelectionsTextures[] = {
-			"\Tenthed_Vehicles\data\Pelican_Variants\pelican_JUNGLE.paa",
-			""
-		};
-	};
-	class Tenthed_Pelican_SOCOM_Urban : OPTRE_Pelican_armed_SOCOM {
-		displayName = "[10th] Pelican SOCOM - Urban";
-		author = "Kodiak";
-		scope = 2;
-		scopeCurator = 2;
-		scopeArsenal = 2;
-
-		faction = "Tenthed_UnitFact_Main";
-		editorCategory = "Tenthed_EditorCategory_Main";
-		editorSubcategory = "Tenthed_EditorSubcategory_Objects_Pelicans";
-
-		hiddenSelectionsTextures[] = {
-			"\Tenthed_Vehicles\data\Pelican_Variants\pelican_URBAN.paa",
-			""
-		};
-	};
-
-	// Old Variant Pelicans
-	class Tenthed_Pelican : Tenthed_Pelican_Base {
-		displayName = "[10th] Pelican";
-		author = "Crow";
-		scope = 1;
-		scopeCurator = 1;
-		scopeArsenal = 1;
-		hiddenSelectionsTextures[] = {
-			"\Tenthed_Vehicles\data\10th_pelican\10th_PelicanExterior.paa",
-			""
-		};
-	};
-	class Tenthed_Archangel_Pelican : Tenthed_Pelican_Base {
-		displayName = "[10th] Archangel Pelican";
-		author = "PocketsTheWizard";
-		scope = 1;
-		scopeCurator = 1;
-		scopeArsenal = 1;
-		hiddenSelectionsTextures[] = {
-			"\Tenthed_Vehicles\data\archangel_pelican\archanglePelican.paa",
-			""
-		};
-	};
-	class Tenthed_Dodo_Pelican : Tenthed_Pelican_Base {
-		displayName = "[10th] DodoAirLines";
-		author = "PocketsTheWizard";
-		scope = 1;
-		scopeCurator = 1;
-		scopeArsenal = 1;
-		hiddenSelectionsTextures[] = {
-			"\Tenthed_Vehicles\data\dodo_pelican\DodoPelican.paa",
-			""
-		};
-	};
 
 	// Base type Hornets
 	class Tenthed_Hornet_Base : OPTRE_UNSC_hornet_green_CAP {
