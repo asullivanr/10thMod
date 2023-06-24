07ccfc47eafea7009111765650fb1a85395bd6cb
Andrew Sullivan <603788+asullivanr@users.noreply.github.com>
2023-06-23 22:03:40 -0500

    Create config.cpp

---
+++ addons/Tenthed_Vehicles/OPFOR/config.cpp
@@ -0,0 +1,12 @@
+class CfgPatches {
+	class Tenthed_OPFOR_Vehicles {
+		author = "Akira";
+		name = "10th ODST: Vehicles";
+		requiredAddons[] = {
+			"Tenthed_core",
+			"OPTRE_Core"
+		};
+		weapons[] = {};
+		units[] = {};
+	};
+};
