5bf95dd21a3416f1055e86b81e7b37d78e09d95e
Andrew Sullivan <603788+asullivanr@users.noreply.github.com>
2023-10-11 00:05:51 -0500

    Update config.cpp
    Added missing recon armor

--- addons/Tenthed_Armor_Misriah_Base/config.cpp
+++ addons/Tenthed_Armor_Misriah_Base/config.cpp
@@ -720,7 +720,63 @@ class cfgWeapons {
 			};
 		};
 	};
-
+	class Tenthed_M56R_Vest_Light : Tenthed_Vest_Base {
+		scope=2;
+		scopeArsenal=2;
+		displayName="[10th] M56R Combat Armor (Light)";
+		model="MA_Armor\data\Vests\H3_ODST\H3_ODST_Armor.p3d";
+		picture="MA_Armor\data\Icons\H3ODST_Vest.paa";
+		hiddenSelectionsTextures[]= {
+			"MA_Armor\data\Vests\H3_ODST\H3_Upper_Armor_co.paa",
+			"MA_Armor\data\Vests\H3_ODST\H3_Lower_Armor_co.paa",
+			"MA_Armor\data\Vests\H3_ODST\Addons\Thigh_Pouch_co.paa",
+			"MA_Armor\data\Vests\Reach_ODST\Reach_Armor_Upper_co.paa",
+			"MA_Armor\data\Vests\Reach_ODST\Reach_Armor_Lower_co.paa",
+			"MA_Armor\data\Vests\Reach_ODST\Canisters_co.paa",
+			"MA_Armor\data\Vests\Reach_ODST\Forearm_Vents_co.paa",
+			"MA_Armor\data\Vests\Reach_ODST\Addons\Sniper_Shoulders\Sniper_co.paa",
+			"MA_Armor\data\Vests\Reach_ODST\Addons\CQB_Shoulders\CQB_co.paa",
+			"MA_Armor\data\Vests\Reach_ODST\Addons\Reach_Shoulder_Radio\Shoulder_Radio_Green_co.paa"
+		};
+		class ItemInfo: ItemInfo {
+			vestType="Rebreather";
+			uniformModel="MA_Armor\data\Vests\H3_ODST\H3_ODST_Armor.p3d";
+			containerClass="Supply350";
+			hiddenSelections[]= { // put a // in front of section to show the item. by default the right sniper pauldron is shown and the CQB left pauldron is shown.
+				"camo1",
+				"camo2",
+				"camo3",
+				"camo4",
+				"camo5",
+				"camo6",
+				"camo7",
+				"camo8",
+				"camo9",
+				"camo10",
+				"H3_Forearm_Left",
+				"H3_Forearm_Right",
+				"Reach_Forearm_Left",
+				"Reach_Forearm_Right",
+				"H3_Pauldron_Left",
+				"H3_Pauldron_Right",
+				"Reach_Pauldron_Left",
+				"Reach_Pauldron_Right",
+				"Reach_Shoulder_Radio_Left",
+				"Reach_Shoulder_Radio_Right",
+				"Reach_Sniper_Pauldron_Left",
+				"Reach_Sniper_Pauldron_Right",
+				"Reach_CQB_Pauldron_Left",
+				"Reach_CQB_Pauldron_Right",
+				"Thigh_Pouch",
+				"H3_Armor_Upper",
+				"H3_Armor_Lower",
+				"Forearm_Vent_Left",
+				"Forearm_Vent_Right",
+				"Canisters"
+			};
+		};
+	};
+	
 	// Enlisted Variants
 	// Enlisted Rifleman - no radio
 	class Tenthed_M56R_Vest_Enlisted_Rifleman : Tenthed_Vest_Base {
