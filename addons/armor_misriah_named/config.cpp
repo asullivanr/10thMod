34bd3ade1cd72b4e8a358515a5c259125c4c6353
Andrew Sullivan <603788+asullivanr@users.noreply.github.com>
2024-02-17 00:20:37 -0600

    Created armor misriah named

---
+++ addons/armor_misriah_named/config.cpp
@@ -0,0 +1,231 @@
+class CfgPatches {
+	class Tenthed_armor_misriah_named {
+		author = "Akira, Tiny";
+		name = "10th Armor - Misriah Named";
+		requiredAddons[] = {
+			"MA_Armor",
+			"Tenthed_Armor_Misriah_Base"
+		};
+		weapons[] = {};
+		units[] = {};
+	};
+};
+
+class cfgWeapons {
+	class HeadgearItem;
+	class ItemCore;
+	class InventoryItem_Base_F;
+	class VestItem;
+	class V_PlateCarrier1_rgr;
+	class U_B_CombatUniform_mcam;
+	class UniformItem;
+	class NVGoggles;
+	class HitPoints;
+	class Uniform_Base: ItemCore {
+		class ItemInfo;
+	};
+	class ItemInfo;
+
+	class Tenthed_M56SR_Helmet;
+	class Tenthed_M56A_Helmet;
+	class Tenthed_M56G_Helmet;
+	class Tenthed_M56E_Helmet;
+	class Tenthed_M56_Scout_Helmet;
+
+	class Tenthed_Vest_Base;
+
+	// Akira
+	#include "data\Akira\armor.hpp"
+
+	// Tiny
+	#include "data\Tiny\armor.hpp"
+
+	// Lion
+	#include "data\Lion\armor.hpp"
+
+	// Nova
+	#include "data\Nova\armor.hpp"
+
+	// Villainess
+	#include "data\Villainess\armor.hpp"
+	
+	// Void
+	#include "data\Void\armor.hpp"
+
+	// Mac
+	#include "data\Mac\armor.hpp"
+	
+	// Bbstine
+	#include "data\Bbstine\armor.hpp"
+
+	// Flipper
+	#include "data\Flipper\armor.hpp"
+
+	// Rambler
+	#include "data\Rambler\armor.hpp"
+
+	// Slim
+	#include "data\Slim\armor.hpp"
+
+	// Boomstick
+	#include "data\Boomstick\armor.hpp"
+    
+    // Blindwolf
+	#include "data\Blindwolf\armor.hpp"
+
+	// Jad
+	#include "data\Jad\armor.hpp"
+//
+	// Clem
+	#include "data\Clem\armor.hpp"
+
+	// Cookie
+	#include "data\Cookie\armor.hpp"
+
+	// Dovah
+	#include "data\Dovah\armor.hpp"
+
+	// Drake
+	#include "data\Drake\armor.hpp"
+
+	// Gutts
+	#include "data\Gutts\armor.hpp"
+
+	// HardDrop
+	#include "data\HardDrop\armor.hpp"
+
+	// Hatchet
+	#include "data\Hatchet\armor.hpp"
+
+	// Leek
+	#include "data\Leek\armor.hpp"
+
+	// Nobody
+	#include "data\Nobody\armor.hpp"
+
+	// Seagull
+	#include "data\Seagull\armor.hpp"
+
+	// Som
+	#include "data\Som\armor.hpp"
+//
+	// Cossack
+	#include "data\Cossack\armor.hpp"
+
+	// Evader
+	#include "data\Evader\armor.hpp"
+
+	// Freedom
+	#include "data\Freedom\armor.hpp"
+
+	// Goldy
+	#include "data\Goldy\armor.hpp"
+
+	// Grimgnaw
+	#include "data\Grimgnaw\armor.hpp"
+
+	// Hodge
+	#include "data\Hodge\armor.hpp"
+
+	// Infra
+	#include "data\Infra\armor.hpp"
+
+	// McInnes
+	#include "data\McInnes\armor.hpp"
+
+	// Meatshield
+	#include "data\Meatshield\armor.hpp"
+
+	// Tiger
+	#include "data\Tiger\armor.hpp"
+//
+	// Atlas
+	#include "data\Atlas\armor.hpp"
+
+	// Dex
+	#include "data\Dex\armor.hpp"
+
+	// Dubbos
+	#include "data\Dubbos\armor.hpp"
+
+	// Fwog
+	#include "data\Fwog\armor.hpp"
+
+	// Glint
+	#include "data\Glint\armor.hpp"
+
+	// Jody
+	#include "data\Jody\armor.hpp"
+
+	// Marvel
+	#include "data\Marvel\armor.hpp"
+
+	// Mom
+	#include "data\Mom\armor.hpp"
+
+	// Optional
+	#include "data\Optional\armor.hpp"
+
+	// Storm
+	#include "data\Storm\armor.hpp"
+//
+	// Beans
+	#include "data\Beans\armor.hpp"
+
+	// Blasphemy
+	#include "data\Blasphemy\armor.hpp"
+
+	// Hetzen
+	#include "data\Hetzen\armor.hpp"
+
+	// Jaeger
+	#include "data\Jaeger\armor.hpp"
+
+	// Knight
+	#include "data\Knight\armor.hpp"
+
+	// Mage
+	#include "data\Mage\armor.hpp"
+
+	// Metro
+	#include "data\Metro\armor.hpp"
+
+	// Xyric
+	#include "data\Xyric\armor.hpp"
+//
+	// Den
+	#include "data\Den\armor.hpp"
+
+	// Naffen
+	#include "data\Naffen\armor.hpp"
+//
+	// Cheeto
+	#include "data\Cheeto\armor.hpp"
+
+	// Failure
+	#include "data\Failure\armor.hpp"
+
+	// Goober
+	#include "data\Goober\armor.hpp"
+
+	// Radical
+	#include "data\Radical\armor.hpp"
+
+	// Rook
+	#include "data\Rook\armor.hpp"
+//
+	// Fynn
+	#include "data\Fynn\armor.hpp"
+
+	// Life
+	#include "data\Life\armor.hpp"
+
+	// Rad
+	#include "data\Rad\armor.hpp"
+
+	// Silver
+	#include "data\Silver\armor.hpp"
+
+	// Vex
+	#include "data\Vex\armor.hpp"
+};
