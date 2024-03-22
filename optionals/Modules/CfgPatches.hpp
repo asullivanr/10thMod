69a033a8b1c32c1746ac7d2c6ea9f02f5d58f0a7
Andrew Sullivan <603788+asullivanr@users.noreply.github.com>
2024-03-22 13:14:41 -0500

    Drop pod testing

---
+++ optionals/Modules/CfgPatches.hpp
@@ -0,0 +1,13 @@
+class CfgPatches {
+	class Tenthed_Modules {
+		author = "Akira";
+		name = "10th Modules Test";
+		requiredAddons[] = {"OPTRE_Modules"};
+		weapons[] = {};
+		units[] = 
+		{
+			"Module_Tenthed_ODSTDrop",
+			"Tenthed_ODSTDrop",
+		};
+	};
+};
