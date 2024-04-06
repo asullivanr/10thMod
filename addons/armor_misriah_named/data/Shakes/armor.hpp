d8b8be68a6be86fb656c83d84194499a86e0aeba
Anxxnymous <p4nd4girl0862@gmail.com>
2024-04-06 18:29:00 -0400

    Update 3.7
    Added
    - Dinner
    - Shakes
    - Mage Legs
    - Dubbos Legs
    
    Edited
    - Vex Full Set
    - Mac Full Set
    - Bbstine Legs

--- addons/armor_misriah_named/data/Shakes/armor.hpp
+++ addons/armor_misriah_named/data/Shakes/armor.hpp
@@ -1,21 +1,20 @@
-	
-    class Tenthed_M56R_Vest_Dubbos : Tenthed_Vest_Base {
+	class Tenthed_M56R_Vest_Shakes : Tenthed_Vest_Base {
 		scope=2;
 		scopeArsenal=2;
-		displayName="[10th] M56R Armor (Dubbos)";
+		displayName="[10th] M56R Armor (Shakes)";
 		model="MA_Armor\data\Vests\H3_ODST\H3_ODST_Armor.p3d";
 		picture="MA_Armor\data\Icons\H3ODST_Vest.paa";
-		hiddenSelectionsTextures[]= { // Change folder to Armor-Misriah\data and put files into that folder. example "Armor-Misriah\data\akira_helmet.paa"
+		hiddenSelectionsTextures[]= { // Change folder to Armor-Misriah\data and put files into that folder. example "Armor-Misriah\data\Leek_helmet.paa"
 			// No other texture files then the ones with comments will be altered.
 			"MA_Armor\data\Vests\H3_ODST\H3_Upper_Armor_co.paa",
 			"MA_Armor\data\Vests\H3_ODST\H3_Lower_Armor_co.paa",
-			"z\10thMod\addons\armor_misriah_named\data\Dubbos\Thigh_Pouch_Dubbos_co.paa", // Thigh pouch texture
-			"z\10thMod\addons\armor_misriah_named\data\Dubbos\Reach_Armor_Upper_Dubbos_co.paa", // Upper armor texture and base ODST shoulders.
-			"z\10thMod\addons\armor_misriah_named\data\Dubbos\Reach_Armor_Lower_Dubbos_co.paa",
+			"MA_Armor\data\Vests\H3_ODST\Addons\Thigh_Pouch_co.paa", // Thigh pouch texture
+			"z\10thMod\addons\armor_misriah_named\data\Shakes\Reach_Armor_Upper_Shakes_co.paa", // Upper armor texture and base ODST shoulders.
+			"MA_Armor\data\Vests\Reach_ODST\Reach_Armor_Lower_co.paa",
 			"MA_Armor\data\Vests\Reach_ODST\Canisters_co.paa", // Canisters texture
 			"MA_Armor\data\Vests\Reach_ODST\Forearm_Vents_co.paa",
 			"MA_Armor\data\Vests\Reach_ODST\Addons\Sniper_Shoulders\Sniper_co.paa", // Sniper Shoulders
-			"MA_Armor\data\Vests\Reach_ODST\Addons\CQB_Shoulders\CQB_co.paa", // CQB Shoulders
+			"z\10thMod\addons\armor_misriah_named\data\Shakes\CQB_Shakes_co.paa", // CQB Shoulders
 			"MA_Armor\data\Vests\Reach_ODST\Addons\Reach_Shoulder_Radio\Shoulder_Radio_ODST_co.paa"
 		};
 		class ItemInfo: ItemInfo {
@@ -39,15 +38,15 @@
 				//"Reach_Forearm_Right", // if the forearms are enabled they must have the vents
 				"H3_Pauldron_Left", // not used
 				"H3_Pauldron_Right", // not used
-				//"Reach_Pauldron_Left",
-				//"Reach_Pauldron_Right",
+				"Reach_Pauldron_Left",
+				"Reach_Pauldron_Right",
 				"Reach_Shoulder_Radio_Left",
 				"Reach_Shoulder_Radio_Right",
 				"Reach_Sniper_Pauldron_Left",
 				"Reach_Sniper_Pauldron_Right",
-				"Reach_CQB_Pauldron_Left",
-				"Reach_CQB_Pauldron_Right",
-				//"Thigh_Pouch",
+				//"Reach_CQB_Pauldron_Left",
+				//"Reach_CQB_Pauldron_Right",
+				"Thigh_Pouch",
 				"H3_Armor_Upper", // not used
 				"H3_Armor_Lower", // not used
 				//"Forearm_Vent_Left",  // if the forearms are enabled they must have the vents
@@ -58,21 +57,21 @@
 	};
 
 
-	class Tenthed_M56SR_Helmet_Dubbos : Tenthed_M56SR_Helmet {
+	class Tenthed_M56E_Helmet_Shakes : Tenthed_M56E_Helmet {
 		scope=2;
 		scopeArsenal=2;
-		displayName="[10th] M56S-R Helmet (Dubbos)";
-		model="MA_Armor\data\Helmets\HR_ODST\HR_ODST.p3d";
-		picture="MA_Armor\data\Icons\Halo_Reach_ODST_Helmet.paa";
+		displayName="[10th] M56-E Helmet (Shakes)";
+		model="MA_Armor\data\Helmets\EOD\EOD_Helmet.p3d";
+		picture="MA_Armor\data\Icons\EOD_Helm.paa";
 		hiddenSelectionsTextures[]= { // un comment out which visor color you would like to test. make sure to comment out the old color or else there will be errors. Blue visor enabled by default.
-			"z\10thMod\addons\armor_misriah_named\data\Dubbos\HR_ODST_Helmet_Dubbos_co.paa", // change this texture file to test different helmet textures.
-			//"MA_Armor\data\Helmets\HR_ODST\HR_ODST_Visor_Blue_co.paa" // Blue Visor
-			"MA_Armor\data\Helmets\HR_ODST\HR_ODST_Visor_black_co.paa" // Black Visor
-			//"MA_Armor\data\Helmets\HR_ODST\HR_ODST_Visor_gold_co.paa" // Gold Visor
-			//"MA_Armor\data\Helmets\HR_ODST\HR_ODST_Visor_red_co.paa" // Red Visor
-			//"MA_Armor\data\Helmets\HR_ODST\HR_ODST_Visor_white_co.paa" // White Visor
+			"z\10thMod\addons\armor_misriah_named\data\Shakes\EOD_Helmet_Shakes_co.paa", // change this texture file to test different helmet textures.
+			//"MA_Armor\data\Helmets\EOD\EOD_Visor_Blue_co.paa" // Blue Visor
+			//"MA_Armor\data\Helmets\EOD\EOD_Visor_Black_co.paa" // Black Visor
+			//"MA_Armor\data\Helmets\EOD\EOD_Visor_Gold_co.paa" // Gold Visor
+			//"MA_Armor\data\Helmets\EOD\EOD_Visor_Red_co.paa" // Red Visor
+			"MA_Armor\data\Helmets\EOD\EOD_Visor_White_co.paa" // White Visor
 		};
 		class ItemInfo: ItemInfo {
-			uniformModel="MA_Armor\data\Helmets\HR_ODST\HR_ODST.p3d";
+			uniformModel="MA_Armor\data\Helmets\EOD\EOD_Helmet.p3d";
 		};
 	};
--- addons/armor_misriah_named/data/Vex/armor.hpp
+++ addons/armor_misriah_named/data/Vex/armor.hpp
@@ -1,5 +1,5 @@
 	class Tenthed_M56A_Helmet_Vex : Tenthed_M56A_Helmet {
-		scope=2;
+		scope=0;
 		scopeArsenal=2;
 		displayName="[10th] M56-A Helmet (Vex)";
 		model="MA_Armor\data\Helmets\AA\AA_Helmet.p3d";
@@ -53,13 +53,13 @@
 				"camo10",
 				"H3_Forearm_Left", // not used
 				"H3_Forearm_Right", // not used
-				"Reach_Forearm_Left",  // if the forearms are enabled they must have the vents
-				"Reach_Forearm_Right", // if the forearms are enabled they must have the vents
+				//"Reach_Forearm_Left",  // if the forearms are enabled they must have the vents
+				//"Reach_Forearm_Right", // if the forearms are enabled they must have the vents
 				"H3_Pauldron_Left", // not used
 				"H3_Pauldron_Right", // not used
-				"Reach_Pauldron_Left",
-				"Reach_Pauldron_Right",
-				"Reach_Shoulder_Radio_Left",
+				//"Reach_Pauldron_Left",
+				//"Reach_Pauldron_Right",
+				//"Reach_Shoulder_Radio_Left",
 				"Reach_Shoulder_Radio_Right",
 				"Reach_Sniper_Pauldron_Left",
 				"Reach_Sniper_Pauldron_Right",
@@ -68,9 +68,29 @@
 				"Thigh_Pouch",
 				"H3_Armor_Upper", // not used
 				"H3_Armor_Lower", // not used
-				"Forearm_Vent_Left",  // if the forearms are enabled they must have the vents
-				"Forearm_Vent_Right", // if the forearms are enabled they must have the vents
-				"Canisters" // the canisters around the waist
+				//"Forearm_Vent_Left",  // if the forearms are enabled they must have the vents
+				//"Forearm_Vent_Right", // if the forearms are enabled they must have the vents
+				//"Canisters" // the canisters around the waist
 			};
+		};
+	};
+
+
+	class Tenthed_M56SR_Helmet_Vex : Tenthed_M56SR_Helmet {
+		scope=2;
+		scopeArsenal=2;
+		displayName="[10th] M56S-R Helmet (Vex)";
+		model="MA_Armor\data\Helmets\HR_ODST\HR_ODST.p3d";
+		picture="MA_Armor\data\Icons\Halo_Reach_ODST_Helmet.paa";
+		hiddenSelectionsTextures[]= { // un comment out which visor color you would like to test. make sure to comment out the old color or else there will be errors. Blue visor enabled by default.
+			"z\10thMod\addons\armor_misriah_named\data\Vex\HR_ODST_Helmet_Vex_co.paa", // change this texture file to test different helmet textures.
+			//"MA_Armor\data\Helmets\HR_ODST\HR_ODST_Visor_Blue_co.paa" // Blue Visor
+			//"MA_Armor\data\Helmets\HR_ODST\HR_ODST_Visor_black_co.paa" // Black Visor
+			//"MA_Armor\data\Helmets\HR_ODST\HR_ODST_Visor_gold_co.paa" // Gold Visor
+			"MA_Armor\data\Helmets\HR_ODST\HR_ODST_Visor_red_co.paa" // Red Visor
+			//"MA_Armor\data\Helmets\HR_ODST\HR_ODST_Visor_white_co.paa" // White Visor
+		};
+		class ItemInfo: ItemInfo {
+			uniformModel="MA_Armor\data\Helmets\HR_ODST\HR_ODST.p3d";
 		};
 	};
