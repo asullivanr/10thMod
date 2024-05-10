a0d1d9bfdae9f2df8bc855ed6792cf81f2b6295a
Anxxnymous <p4nd4girl0862@gmail.com>
2024-05-10 09:24:40 -0400

    Update 3.8
    Added
    Bardok
    Crusader
    Dadbod
    Jody Legs
    Woo
    
    Edited
    Silver Full Set
    
    Fixed
    Dubbos Full Set

--- addons/armor_misriah_named/data/Bardok/armor.hpp
+++ addons/armor_misriah_named/data/Bardok/armor.hpp
@@ -1,25 +1,25 @@
-	class Tenthed_M56SR_Helmet_Silver : Tenthed_M56SR_Helmet {
+	class Tenthed_M56SR_Helmet_Bardok : Tenthed_M56SR_Helmet {
 		scope=2;
 		scopeArsenal=2;
-		displayName="[10th] M56S-R Helmet (Silver)";
+		displayName="[10th] M56S-R Helmet (Bardok)";
 		model="MA_Armor\data\Helmets\HR_ODST\HR_ODST.p3d";
 		picture="MA_Armor\data\Icons\Halo_Reach_ODST_Helmet.paa";
 		hiddenSelectionsTextures[]= { // un comment out which visor color you would like to test. make sure to comment out the old color or else there will be errors. Blue visor enabled by default.
-			"z\10thMod\addons\armor_misriah_named\data\Silver\HR_ODST_Helmet_Silver_co.paa", // change this texture file to test different helmet textures.
+			"z\10thMod\addons\armor_misriah_named\data\Bardok\HR_ODST_Helmet_Bardok_co.paa", // change this texture file to test different helmet textures.
 			//"MA_Armor\data\Helmets\HR_ODST\HR_ODST_Visor_Blue_co.paa" // Blue Visor
-			"MA_Armor\data\Helmets\HR_ODST\HR_ODST_Visor_black_co.paa" // Black Visor
+			//"MA_Armor\data\Helmets\HR_ODST\HR_ODST_Visor_black_co.paa" // Black Visor
 			//"MA_Armor\data\Helmets\HR_ODST\HR_ODST_Visor_gold_co.paa" // Gold Visor
-			//"MA_Armor\data\Helmets\HR_ODST\HR_ODST_Visor_red_co.paa" // Red Visor
+			"MA_Armor\data\Helmets\HR_ODST\HR_ODST_Visor_red_co.paa" // Red Visor
 			//"MA_Armor\data\Helmets\HR_ODST\HR_ODST_Visor_white_co.paa" // White Visor
 		};
 		class ItemInfo: ItemInfo {
 			uniformModel="MA_Armor\data\Helmets\HR_ODST\HR_ODST.p3d";
 		};
 	};
-	class Tenthed_M56R_Vest_Silver : Tenthed_Vest_Base {
+	class Tenthed_M56R_Vest_Bardok : Tenthed_Vest_Base {
 		scope=2;
 		scopeArsenal=2;
-		displayName="[10th] M56R Armor (Silver)";
+		displayName="[10th] M56R Armor (Bardok)";
 		model="MA_Armor\data\Vests\H3_ODST\H3_ODST_Armor.p3d";
 		picture="MA_Armor\data\Icons\H3ODST_Vest.paa";
 		hiddenSelectionsTextures[]= { // Change folder to Armor-Misriah\data and put files into that folder. example "Armor-Misriah\data\akira_helmet.paa"
@@ -27,11 +27,11 @@
 			"MA_Armor\data\Vests\H3_ODST\H3_Upper_Armor_co.paa",
 			"MA_Armor\data\Vests\H3_ODST\H3_Lower_Armor_co.paa",
 			"MA_Armor\data\Vests\H3_ODST\Addons\Thigh_Pouch_co.paa", // Thigh pouch texture
-			"z\10thMod\addons\armor_misriah_named\data\Silver\Reach_Armor_Upper_Silver_co.paa", // Upper armor texture and base ODST shoulders.
+			"z\10thMod\addons\armor_misriah_named\data\Bardok\Reach_Armor_Upper_Bardok_co.paa", // Upper armor texture and base ODST shoulders.
 			"MA_Armor\data\Vests\Reach_ODST\Reach_Armor_Lower_co.paa",
 			"MA_Armor\data\Vests\Reach_ODST\Canisters_co.paa", // Canisters texture
 			"MA_Armor\data\Vests\Reach_ODST\Forearm_Vents_co.paa",
-			"z\10thMod\addons\armor_misriah_named\data\Silver\Sniper_Silver_co.paa", // Sniper Shoulders
+			"z\10thMod\addons\armor_misriah_named\data\Bardok\Sniper_Bardok_co.paa", // Sniper Shoulders
 			"MA_Armor\data\Vests\Reach_ODST\Addons\CQB_Shoulders\CQB_co.paa", // CQB Shoulders
 			"MA_Armor\data\Vests\Reach_ODST\Addons\Reach_Shoulder_Radio\Shoulder_Radio_ODST_co.paa"
 		};
@@ -56,15 +56,15 @@
 				//"Reach_Forearm_Right", // if the forearms are enabled they must have the vents
 				"H3_Pauldron_Left", // not used
 				"H3_Pauldron_Right", // not used
-				//"Reach_Pauldron_Left",
+				"Reach_Pauldron_Left",
 				//"Reach_Pauldron_Right",
 				"Reach_Shoulder_Radio_Left",
 				"Reach_Shoulder_Radio_Right",
-				"Reach_Sniper_Pauldron_Left",
+				//"Reach_Sniper_Pauldron_Left",
 				"Reach_Sniper_Pauldron_Right",
 				"Reach_CQB_Pauldron_Left",
 				"Reach_CQB_Pauldron_Right",
-				"Thigh_Pouch",
+				//"Thigh_Pouch",
 				"H3_Armor_Upper", // not used
 				"H3_Armor_Lower", // not used
 				//"Forearm_Vent_Left",  // if the forearms are enabled they must have the vents
