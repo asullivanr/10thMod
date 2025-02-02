c09dbdb7d803d7aa33bc09d20ddf78d69c4f6e68
Andrew Sullivan <603788+asullivanr@users.noreply.github.com>
2025-02-01 23:13:28 -0600

    update

---
+++ addons/vehicles/Longsword/config.cpp
@@ -0,0 +1,18 @@
+class CfgPatches {
+	class Tenthed_Vehicles_Longsword {
+		author = "Akira";
+		name = "10th ODST: Longsword";
+		requiredAddons[] = {
+			"Tenthed_core",
+			"OPTRE_Core"
+		};
+		weapons[] = {};
+		units[] = {
+			//"Tenthed_Longsword",
+		};
+	};
+};
+class CfgVehicles {
+
+
+};
