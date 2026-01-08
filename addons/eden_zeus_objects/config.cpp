1670bbbb7bb27a5af06de2c6b2dff92d94262c2d
Andrew Sullivan <603788+asullivanr@users.noreply.github.com>
2026-01-08 17:47:55 -0600

    Update config.cpp

--- addons/eden_zeus_objects/config.cpp
+++ addons/eden_zeus_objects/config.cpp
@@ -15,7 +15,7 @@ class CfgVehicles {
 	class Tenthed_CerealBox_Base : Land_CerealsBox_F {
 		scope = 2;
 		scopeCurator = 2;
-		displayName = "ODST CerealBox";
+		displayName = "Trigger CerealBox";
 		editorCategory = "Tenthed_EditorCategory_Objects";
 		editorSubcategory = "Tenthed_EditorSubcategory_Objects_Zeus";
 	};
@@ -39,5 +39,37 @@ class CfgVehicles {
 	};
 	class Tenthed_CerealBox_TriggerOffAA : Tenthed_CerealBox_Base {
 		displayName = "Trigger Disable: AA System";
+	};
+
+	class Tenthed_CerealBox_TriggerCinemaBorders1 : Tenthed_CerealBox_Base {
+		displayName = "Trigger: Cinema Borders, 1";
+	};
+	class Tenthed_CerealBox_TriggerCinemaBorders2 : Tenthed_CerealBox_Base {
+		displayName = "Trigger: Cinema Borders, 2";
+	};
+	class Tenthed_CerealBox_TriggerCinemaBorders3 : Tenthed_CerealBox_Base {
+		displayName = "Trigger: Cinema Borders, 3";
+	};
+	class Tenthed_CerealBox_TriggerCinemaBorders4 : Tenthed_CerealBox_Base {
+		displayName = "Trigger: Cinema Borders, 4";
+	};
+	class Tenthed_CerealBox_TriggerCinemaBorders5 : Tenthed_CerealBox_Base {
+		displayName = "Trigger: Cinema Borders, 5";
+	};
+
+	class Tenthed_CerealBox_TriggerPhase1 : Tenthed_CerealBox_Base {
+		displayName = "Trigger: Phase 1";
+	};
+	class Tenthed_CerealBox_TriggerPhase2 : Tenthed_CerealBox_Base {
+		displayName = "Trigger: Phase 2";
+	};
+	class Tenthed_CerealBox_TriggerPhase3 : Tenthed_CerealBox_Base {
+		displayName = "Trigger: Phase 3";
+	};
+	class Tenthed_CerealBox_TriggerPhase4 : Tenthed_CerealBox_Base {
+		displayName = "Trigger: Phase 4";
+	};
+	class Tenthed_CerealBox_TriggerPhase5 : Tenthed_CerealBox_Base {
+		displayName = "Trigger: Phase 5";
 	};
 };
