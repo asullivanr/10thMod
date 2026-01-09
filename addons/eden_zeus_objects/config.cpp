bf81ab18647bdd91252a54cba31f44a6c03f51a4
Andrew Sullivan <603788+asullivanr@users.noreply.github.com>
2026-01-08 18:05:53 -0600

    Update config.cpp

--- addons/eden_zeus_objects/config.cpp
+++ addons/eden_zeus_objects/config.cpp
@@ -21,55 +21,55 @@ class CfgVehicles {
 	};
 
 	class Tenthed_CerealBox_TriggerOn : Tenthed_CerealBox_Base {
-		displayName = "Trigger Enable: CerealBox";
+		displayName = "Trigger Enable";
 	};
 	class Tenthed_CerealBox_TriggerOff : Tenthed_CerealBox_Base {
-		displayName = "Trigger Disable: CerealBox";
+		displayName = "Trigger Disable";
 	};
 
 	class Tenthed_CerealBox_TriggerOnAirfield : Tenthed_CerealBox_Base {
-		displayName = "Trigger Enable: Airfield";
+		displayName = "Airfield: Trigger Enable";
 	};
 	class Tenthed_CerealBox_TriggerOffAirfield : Tenthed_CerealBox_Base {
-		displayName = "Trigger Disable: Airfield";
+		displayName = "Airfield: Trigger Disable";
 	};
 
 	class Tenthed_CerealBox_TriggerOnAA : Tenthed_CerealBox_Base {
-		displayName = "Trigger Enable: AA System";
+		displayName = "AA System: Trigger Enable";
 	};
 	class Tenthed_CerealBox_TriggerOffAA : Tenthed_CerealBox_Base {
-		displayName = "Trigger Disable: AA System";
+		displayName = "AA System: Trigger Disable";
 	};
 
 	class Tenthed_CerealBox_TriggerCinemaBorders1 : Tenthed_CerealBox_Base {
-		displayName = "Trigger: Cinema Borders, 1";
+		displayName = "Cinema Borders, 1 Trigger";
 	};
 	class Tenthed_CerealBox_TriggerCinemaBorders2 : Tenthed_CerealBox_Base {
-		displayName = "Trigger: Cinema Borders, 2";
+		displayName = "Cinema Borders, 2 Trigger";
 	};
 	class Tenthed_CerealBox_TriggerCinemaBorders3 : Tenthed_CerealBox_Base {
-		displayName = "Trigger: Cinema Borders, 3";
+		displayName = "Cinema Borders, 3 Trigger";
 	};
 	class Tenthed_CerealBox_TriggerCinemaBorders4 : Tenthed_CerealBox_Base {
-		displayName = "Trigger: Cinema Borders, 4";
+		displayName = "Cinema Borders, 4 Trigger";
 	};
 	class Tenthed_CerealBox_TriggerCinemaBorders5 : Tenthed_CerealBox_Base {
-		displayName = "Trigger: Cinema Borders, 5";
+		displayName = "Cinema Borders, 5 Trigger";
 	};
 
 	class Tenthed_CerealBox_TriggerPhase1 : Tenthed_CerealBox_Base {
-		displayName = "Trigger: Phase 1";
+		displayName = "Phase 1 Trigger";
 	};
 	class Tenthed_CerealBox_TriggerPhase2 : Tenthed_CerealBox_Base {
-		displayName = "Trigger: Phase 2";
+		displayName = "Phase 2 Trigger";
 	};
 	class Tenthed_CerealBox_TriggerPhase3 : Tenthed_CerealBox_Base {
-		displayName = "Trigger: Phase 3";
+		displayName = "Phase 3 Trigger";
 	};
 	class Tenthed_CerealBox_TriggerPhase4 : Tenthed_CerealBox_Base {
-		displayName = "Trigger: Phase 4";
+		displayName = "Phase 4 Trigger";
 	};
 	class Tenthed_CerealBox_TriggerPhase5 : Tenthed_CerealBox_Base {
-		displayName = "Trigger: Phase 5";
+		displayName = "Phase 5 Trigger";
 	};
 };
