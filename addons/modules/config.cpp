b0e5684b6d4b662c0e1827e34bb62ad9ffe60a4a
Andrew Sullivan <603788+asullivanr@users.noreply.github.com>
2026-08-11 19:42:31 -0500

    Update config.cpp

--- addons/modules/config.cpp
+++ addons/modules/config.cpp
@@ -11,3 +11,41 @@ class CfgPatches {
         units[] = {};
     };
 };
+class CfgVehicles
+{
+    class Module_F;
+
+    class Tenthed_Module_Arsenal: Module_F
+    {
+        scope = 2;
+        scopeCurator = 2;
+
+        displayName = "Apply Arsenal";
+        icon = "\A3\ui_f\data\igui\cfg\simpleTasks\types\armor_ca.paa";
+        category = "Tenthed_EditorCategory_Modules";
+
+        function = "Tenthed_fnc_ArsenalModule";
+        functionPriority = 1;
+        isGlobal = 2;
+        isTriggerActivated = 0;
+        isDisposable = 1;
+
+        curatorCanAttach = 1;
+        curatorInfoType = "RscCurator";
+
+        class Attributes {};
+    };
+};
+class CfgFunctions
+{
+    class Tenthed
+    {
+        class Functions
+        {
+            class ArsenalModule
+            {
+                file = "functions\fn_ArsenalModule.sqf";
+            };
+        };
+    };
+};
